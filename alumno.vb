de04bcdf52636a2948a4d755da395a7b0700cd0c
cmbelloso <cruzbe96@gmai.com>
2022-12-10 18:19:54 -0600

    se agrega codigo para alumno.vb

--- alumno.vb
+++ alumno.vb
@@ -1,3 +1,49 @@
-﻿Public Class alumno
+﻿Imports Microsoft.VisualBasic
+Imports System.Data
+
+Public Class alumno
+    'instancia de clase conexión
+
+    Dim c As New conexion
+
+    'declaración de propiedades de la clase
+
+    Private codigoalumno As String
+    Private nombrealumno As String
+    Private apellidoalumno As String
+    Private pagina As Page
+
+    'metodos de propiedad
+
+    Public Property codalumno() As String
+        Get
+            Return codigoalumno
+        End Get
+        Set(value As String)
+            codigoalumno = value
+        End Set
+    End Property
+
+    Public Property nomalumno() As String
+        Get
+            Return nombrealumno
+        End Get
+        Set(value As String)
+            nombrealumno = value
+        End Set
+    End Property
+
+    Public Property apealumno() As String
+        Get
+            Return apellidoalumno
+        End Get
+        Set(value As String)
+            apellidoalumno = value
+        End Set
+    End Property
+
+
+
+
 
 End Class
